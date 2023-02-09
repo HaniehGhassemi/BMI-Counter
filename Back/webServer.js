ca42b4c9d508ae08f4ff4d7798f0417c029e0bc6
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-02-09 13:21:11 +0330

    create-webserver

---
+++ Back/webServer.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const port = 3000;
+
+
+app.use('/Front',express.static(path.join(__dirname,'../Front')));
+app.use('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../Front/index.html'));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
