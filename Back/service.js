597a9eac46132c1d38cc9a212e439d873308c399
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-02-10 12:16:08 +0330

    add-bmi-counter-service

---
+++ Back/service.js
@@ -0,0 +1,8 @@
+function bmiCounter (weight,height) {
+    let bmi = weight / height **2;
+    return bmi;
+  
+  }
+
+  
+module.exports.bmiCounter = bmiCounter;
--- Back/webServer.js
+++ Back/webServer.js
@@ -3,10 +3,20 @@ const app = express();
 const path = require('path');
 const port = 3000;
 app.use(express.json());
+const bmi = require('./service')
+
 
 app.use('/',express.static(path.join(__dirname,'../Front')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../Front/index.html'));
+});
+
+
+app.post("/reqBmi", (req, res) => {
+  let weight = req.body.weight;
+  let height = req.body.height;
+  let result = bmi.bmiCounter(weight,height);
+  res.status(200).send({result});
 });
 
 
