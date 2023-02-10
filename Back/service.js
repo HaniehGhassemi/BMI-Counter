61d60756aa49191c15126c695f93c568f9581a16
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-02-10 12:08:28 +0330

    fix-webServer-css-bugs

--- Back/service.js
+++
@@ -1,6 +0,0 @@
-function bmiCounter (weight,height) {
-    let bmi = weight / height **2;
-    return bmi;
-
-}
-export {bmiCounter};
--- Back/webServer.js
+++ Back/webServer.js
@@ -2,13 +2,15 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = 3000;
+app.use(express.json());
 
-app.use('/Front',express.static(path.join(__dirname,'../Front')));
-app.use('/', (req, res) => {
+app.use('/',express.static(path.join(__dirname,'../Front')));
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../Front/index.html'));
-})
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
+});
 //http://localhost:3000/
