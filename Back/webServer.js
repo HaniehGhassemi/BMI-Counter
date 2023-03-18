9bc460ab96920c0ae51ad19e95a1be4cccbe1edb
Hanieh Ghassemi <89027450+HaniehGhassemi@users.noreply.github.com>
2023-03-18 11:08:29 +0330

    Reformat: use prettier extention prettier is extention in vscode that helps the abilities

--- Back/webServer.js
+++ Back/webServer.js
@@ -1,26 +1,23 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 3000;
 app.use(express.json());
-const bmi = require('./service')
+const bmi = require("./service");
 
-
-app.use('/',express.static(path.join(__dirname,'../Front')));
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../Front/index.html'));
+app.use("/", express.static(path.join(__dirname, "../Front")));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Front/index.html"));
 });
-
 
 app.post("/counter", (req, res) => {
   let weight = req.body.weight;
   let height = req.body.height;
-  let result = bmi.bmiCounter(weight,height);
-  res.status(200).send({result});
+  let result = bmi.bmiCounter(weight, height);
+  res.status(200).send({ result });
 });
 
-
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
 //http://localhost:3000/
--- Front/index.js
+++ Front/index.js
@@ -1,35 +1,35 @@
-const btn = document.getElementById('btn');
+const btn = document.getElementById("btn");
 
-btn.addEventListener('click', () => {
-  const form = document.getElementById('form');
-  const answer = document.getElementById('answer');
+btn.addEventListener("click", () => {
+  const form = document.getElementById("form");
+  const answer = document.getElementById("answer");
 
-  if (form.style.display === 'none') {
-    form.style.display = 'block';
-    answer.style.display = 'none';
-
+  if (form.style.display === "none") {
+    form.style.display = "block";
+    answer.style.display = "none";
   } else {
-    form.style.display = 'none';
-    answer.style.display = 'block';
+    form.style.display = "none";
+    answer.style.display = "block";
   }
 });
 
-btn.addEventListener('click', function() {
-    const data = {"height": document.getElementById("height").value,
-    "weight": document.getElementById("weight").value};
+btn.addEventListener("click", function () {
+  const data = {
+    height: document.getElementById("height").value,
+    weight: document.getElementById("weight").value,
+  };
 
-fetch ('/counter' , {
-    method: 'POST',
+  fetch("/counter", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     body: JSON.stringify(data),
   })
     .then((response) => response.json())
     .then((data) => {
-        document.getElementById("answer").innerHTML = `Your BMI is ${data.result}`
-    })
-})
-
-
-
+      document.getElementById(
+        "answer"
+      ).innerHTML = `Your BMI is ${data.result}`;
+    });
+});
